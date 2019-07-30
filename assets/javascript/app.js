c6394af170ee764af01bc95c3613730d7ecd8790
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2019-07-30 16:14:57 +0800

    up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 var eyeBalls = document.getElementsByClassName("eyeball")
-document.onmousemove = function () {
-  var x = event.clientX * 100 / window.innerWidth + "%";
-  var y = event.clientY * 100 / window.innerHeight + "%";
+document.onmousemove = function() {
+    var x = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
 
-  for (var i = 0; i < 2; i++) {
-    eyeBalls[i].style.left = x;
-    eyeBalls[i].style.top = y;
-    eyeBalls[i].style.transform = "translate(-" + x + ",-" + y + ")";
+    for (var i = 0; i < 2; i++) {
+        eyeBalls[i].style.left = x;
+        eyeBalls[i].style.top = y;
+        eyeBalls[i].style.transform = "translate(-" + x + ",-" + y + ")";
 
-  }
+    }
 };
 
 // function on() {
@@ -19,77 +19,77 @@ document.onmousemove = function () {
 //   document.getElementById("overlay-me").style.display = "none";
 // }
 
-$("#btnid1").click(function () {
-  $("#overlay-me").fadeIn(500);
+$("#btnid1").click(function() {
+    $("#overlay-me").fadeIn(500);
 });
 
-$("#portfoliomodal1").click(function(){
-  $("#overlay-portfolio").fadeIn(500);
-  $("#overlay-me").fadeOut(500);
+$("#portfoliomodal1").click(function() {
+    $("#overlay-portfolio").fadeIn(500);
+    $("#overlay-me").fadeOut(500);
 })
 
-$("#contactmodal1").click(function(){
-  $("#overlay-contact").fadeIn(500);
-  $("#overlay-me").fadeOut(500);
+$("#contactmodal1").click(function() {
+    $("#overlay-contact").fadeIn(500);
+    $("#overlay-me").fadeOut(500);
 })
 
 
 // circle2
-$("#btnid2").click(function () {
-  $("#overlay-me").fadeOut(500);
-  $("#overlay-portfolio").fadeIn(500);
+$("#btnid2").click(function() {
+    $("#overlay-me").fadeOut(500);
+    $("#overlay-portfolio").fadeIn(500);
 
 });
 
-$(".footer2").click(function () {
-  $("#overlay-portfolio").fadeOut(500);
+$(".footer2").click(function() {
+    $("#overlay-portfolio").fadeOut(500);
 });
 
-$(".footer2").click(function () {
-  $("#overlay-me").fadeOut(500);
+$(".footer2").click(function() {
+    $("#overlay-me").fadeOut(500);
 });
 
 
 
 // circle3
 
-$(".footer2").click(function () {
-  $("#overlay-contact").fadeOut(500);
+$(".footer2").click(function() {
+    $("#overlay-contact").fadeOut(500);
 });
 
-$(".footer3").click(function () {
-  $("#overlay-contact").fadeOut(500);
-  window.location.reload();
+$(".footer3").click(function() {
+    $("#overlay-contact").fadeOut(500);
+    window.location.reload();
 });
 
-$("#btnid3").click(function () {
-  $("#overlay-contact").fadeIn(500);
-  $("#overlay-me").fadeOut(500);
-  $("#overlay-portfolio").fadeOut(500);
-
-});
-
-$("#btnresumeid").click(function () {
-  $("#overlayresume").fadeIn(500);
+$("#btnid3").click(function() {
+    $("#overlay-contact").fadeIn(500);
+    $("#overlay-me").fadeOut(500);
+    $("#overlay-portfolio").fadeOut(500);
 
 });
 
+$("#btnresumeid").click(function() {
+    $("#overlayresume").fadeIn(500);
 
-$("#btnresumeidhide").click(function () {
-  $("#overlayresume").fadeOut(500);
+});
+
+
+$("#btnresumeidhide").click(function() {
+    $("#overlayresume").fadeOut(500);
 
 });
 
 function submitForm() {
 
-  // document.getElementById("contact-form").submit();
-  var p = document.getElementById('subjectid')
-  p.value = p.value.replace(/\+/g,' ');
-  
-  setTimeout( function( ){
-    document.getElementById("contact-form").reset();
-  }, 3000)
- 
+    // document.getElementById("contact-form").submit();
+    var p = document.getElementById('subjectid')
+    p.value = p.value.replace(/\+/g, ' ');
+
+    setTimeout(function() {
+        document.getElementById("contact-form").reset();
+    }, 3000)
+
 
 }
 
